c897e7224a973f2b2fd4dde289d6410610bfd4f0
Dũng Prj <89343958+Dungprj@users.noreply.github.com>
2023-11-23 00:21:04 +0700

    merge manager_user_p2 to Dung98

---
+++ shoppe-thongtin-html/render_dia_chi.py
@@ -0,0 +1,130 @@
+
+
+
+
+document.querySelector("#modal > aside > div.T91LAk > div > form > div > div.U\\+ue2m > div:nth-child(2) > div > div > div > div.y-umZ1._8bFOAb").click()
+var arr = document.querySelectorAll(".cyDqs3")
+
+
+const innerTextArray = [];
+let huyen =[]
+
+
+
+
+arr.forEach(element => {
+    innerTextArray.push(element.innerText);
+   
+
+
+
+});
+
+console.log(innerTextArray)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+thanh_pho = ['An Giang', 'Bà Rịa - Vũng Tàu', 'Bình Dương', 'Bình Phước', 'Bình Thuận', 'Bình Định', 'Bạc Liêu', 'Bắc Giang', 'Bắc Kạn', 'Bắc Ninh', 'Bến Tre', 'Cao Bằng', 'Cà Mau', 'Cần Thơ', 'Gia Lai', 'Hà Giang', 'Hà Nam', 'Hà Nội', 'Hà Tĩnh', 'Hòa Bình', 'Hưng Yên', 'Hải Dương', 'Hải Phòng', 'Hậu Giang', 'Khánh Hòa', 'Kiên Giang', 'Kon Tum', 'Lai Châu', 'Long An', 'Lào Cai', 'Lâm Đồng', 'Lạng Sơn', 'Nam Định', 'Nghệ An', 'Ninh Bình', 'Ninh Thuận', 'Phú Thọ', 'Phú Yên', 'Quảng Bình', 'Quảng Nam', 'Quảng Ngãi', 'Quảng Ninh', 'Quảng Trị', 'Sóc Trăng', 'Sơn La', 'TP. Hồ Chí Minh', 'Thanh Hóa', 'Thái Bình', 'Thái Nguyên', 'Thừa Thiên Huế', 'Tiền Giang', 'Trà Vinh', 'Tuyên Quang', 'Tây Ninh', 'Vĩnh Long', 'Vĩnh Phúc', 'Yên Bái', 'Điện Biên', 'Đà Nẵng', 'Đắk Lắk', 'Đắk Nông', 'Đồng Nai', 'Đồng Tháp']
+h_1 = ['Huyện An Phú', 'Huyện Châu Phú', 'Huyện Châu Thành', 'Huyện Chợ Mới', 'Huyện Phú Tân', 'Huyện Thoại Sơn', 'Huyện Tri Tôn', 'Huyện Tịnh Biên', 'Thành Phố Châu Đốc', 'Thành Phố Long Xuyên', 'Thị Xã Tân Châu']
+h_2 = ['Huyện Châu Đức', 'Huyện Côn Đảo', 'Huyện Long Điền', 'Huyện Xuyên Mộc', 'Huyện Đất Đỏ', 'Thành Phố Bà Rịa', 'Thành Phố Vũng Tàu', 'Thị Xã Phú Mỹ']
+h_3 = ['Huyện Bàu Bàng', 'Huyện Bắc Tân Uyên', 'Huyện Dầu Tiếng', 'Huyện Phú Giáo', 'Thành Phố Dĩ An', 'Thành Phố Thuận An', 'Thành Phố Thủ Dầu Một', 'Thị Xã Bến Cát', 'Thị Xã Tân Uyên']
+h_4 = ['Huyện Bù Gia Mập', 'Huyện Bù Đăng', 'Huyện Bù Đốp', 'Huyện Chơn Thành', 'Huyện Hớn Quản', 'Huyện Lộc Ninh', 'Huyện Phú Riềng', 'Huyện Đồng Phú', 'Thành Phố Đồng Xoài', 'Thị Xã Bình Long', 'Thị Xã Phước Long']
+h_5 = ['Huyện Bắc Bình', 'Huyện Hàm Thuận Bắc', 'Huyện Hàm Thuận Nam', 'Huyện Hàm Tân', 'Huyện Phú Quí', 'Huyện Tuy Phong', 'Huyện Tánh Linh', 'Huyện Đức Linh', 'Thành Phố Phan Thiết', 'Thị Xã La Gi']
+h_6 = ['Huyện An Lão', 'Huyện Hoài Ân', 'Huyện Phù Cát', 'Huyện Phù Mỹ', 'Huyện Tuy Phước', 'Huyện Tây Sơn', 'Huyện Vân Canh', 'Huyện Vĩnh Thạnh', 'Thành Phố Quy Nhơn', 'Thị Xã An Nhơn', 'Thị Xã Hoài Nhơn']
+h_7 = ['Huyện Hòa Bình', 'Huyện Hồng Dân', 'Huyện Phước Long', 'Huyện Vĩnh Lợi', 'Huyện Đông Hải', 'Thành Phố Bạc Liêu', 'Thị Xã Giá Rai']
+h_8 = ['Huyện Hiệp Hòa', 'Huyện Lạng Giang', 'Huyện Lục Nam', 'Huyện Lục Ngạn', 'Huyện Sơn Động', 'Huyện Tân Yên', 'Huyện Việt Yên', 'Huyện Yên Dũng', 'Huyện Yên Thế', 'Thành Phố Bắc Giang']
+h_9 = ['Huyện Ba Bể', 'Huyện Bạch Thông', 'Huyện Chợ Mới', 'Huyện Chợ Đồn', 'Huyện Na Rì', 'Huyện Ngân Sơn', 'Huyện Pắc Nặm', 'Thành Phố Bắc Kạn']
+h_10 = ['Huyện Gia Bình', 'Huyện Lương Tài', 'Huyện Quế Võ', 'Huyện Thuận Thành', 'Huyện Tiên Du', 'Huyện Yên Phong', 'Thành Phố Bắc Ninh', 'Thành Phố Từ Sơn']
+
+
+h_11 = ['Huyện Ba Tri', 'Huyện Bình Đại', 'Huyện Châu Thành', 'Huyện Chợ Lách', 'Huyện Giồng Trôm', 'Huyện Mỏ Cày Bắc', 'Huyện Mỏ Cày Nam', 'Huyện Thạnh Phú', 'Thành Phố Bến Tre']
+h_12 = ['Huyện Bảo Lâm', 'Huyện Bảo Lạc', 'Huyện Hà Quảng', 'Huyện Hòa An', 'Huyện Hạ Lang', 'Huyện Nguyên Bình', 'Huyện Quảng Hòa', 'Huyện Thạch An', 'Huyện Trùng Khánh', 'Thành Phố Cao Bằng']
+h_13 = ['Huyện Cái Nước', 'Huyện Ngọc Hiển', 'Huyện Năm Căn', 'Huyện Phú Tân', 'Huyện Thới Bình', 'Huyện Trần Văn Thời', 'Huyện U Minh', 'Huyện Đầm Dơi', 'Thành Phố Cà Mau']
+h_14 =  ['Huyện Cờ Đỏ', 'Huyện Phong Điền', 'Huyện Thới Lai', 'Huyện Vĩnh Thạnh', 'Quận Bình Thủy', 'Quận Cái Răng', 'Quận Ninh Kiều', 'Quận Thốt Nốt', 'Quận Ô Môn']
+h_15 = ['Huyện Chư Prông', 'Huyện Chư Păh', 'Huyện Chư Pưh', 'Huyện Chư Sê', 'Huyện Ia Grai', 'Huyện Ia Pa', 'Huyện Kbang', 'Huyện Krông Pa', 'Huyện Kông Chro', 'Huyện Mang Yang', 'Huyện Phú Thiện', 'Huyện Đăk Pơ', 'Huyện Đăk Đoa', 'Huyện Đức Cơ', 'Thành Phố Pleiku', 'Thị Xã An Khê', 'Thị Xã Ayun Pa']
+h_16 = ['Huyện Bắc Mê', 'Huyện Bắc Quang', 'Huyện Hoàng Su Phì', 'Huyện Mèo Vạc', 'Huyện Quang Bình', 'Huyện Quản Bạ', 'Huyện Vị Xuyên', 'Huyện Xín Mần', 'Huyện Yên Minh', 'Huyện Đồng Văn','Thành Phố Hà Giang']
+h_17 = ['Huyện Bình Lục', 'Huyện Kim Bảng', 'Huyện Lý Nhân', 'Huyện Thanh Liêm', 'Thành Phố Phủ Lý', 'Thị Xã Duy Tiên']
+h_18 = ['Huyện Ba Vì', 'Huyện Chương Mỹ', 'Huyện Gia Lâm', 'Huyện Hoài Đức', 'Huyện Mê Linh', 'Huyện Mỹ Đức', 'Huyện Phú Xuyên', 'Huyện Phúc Thọ', 'Huyện Quốc Oai', 'Huyện Sóc Sơn', 'Huyện Thanh Oai', 'Huyện Thanh Trì', 'Huyện Thường Tín', 'Huyện Thạch Thất', 'Huyện Đan Phượng', 'Huyện Đông Anh', 'Huyện Ứng Hòa', 'Quận Ba Đình', 'Quận Bắc Từ Liêm', 'Quận Cầu Giấy', 'Quận Hai Bà Trưng', 'Quận Hoàn Kiếm', 'Quận Hoàng Mai', 'Quận Hà Đông', 'Quận Long Biên', 'Quận Nam Từ Liêm', 'Quận Thanh Xuân', 'Quận Tây Hồ', 'Quận Đống Đa', 'Thị Xã Sơn Tây']
+h_19 = ['Huyện Can Lộc', 'Huyện Cẩm Xuyên', 'Huyện Hương Khê', 'Huyện Hương Sơn', 'Huyện Kỳ Anh', 'Huyện Lộc Hà', 'Huyện Nghi Xuân', 'Huyện Thạch Hà', 'Huyện Vũ Quang', 'Huyện Đức Thọ', 'Thành Phố Hà Tĩnh', 'Thị Xã Hồng Lĩnh', 'Thị Xã Kỳ Anh']
+h_20 = ['Huyện Cao Phong', 'Huyện Kim Bôi', 'Huyện Lương Sơn', 'Huyện Lạc Sơn', 'Huyện Lạc Thủy', 'Huyện Mai Châu', 'Huyện Tân Lạc', 'Huyện Yên Thủy', 'Huyện Đà Bắc', 'Thành Phố Hòa Bình']
+
+
+h_21 = ['Huyện Khoái Châu', 'Huyện Kim Động', 'Huyện Phù Cừ', 'Huyện Tiên Lữ', 'Huyện Văn Giang', 'Huyện Văn Lâm', 'Huyện Yên Mỹ', 'Huyện Ân Thi', 'Thành Phố Hưng Yên', 'Thị Xã Mỹ Hào']
+h_22 = ['Huyện Bình Giang', 'Huyện Cẩm Giàng', 'Huyện Gia Lộc', 'Huyện Kim Thành', 'Huyện Nam Sách', 'Huyện Ninh Giang', 'Huyện Thanh Hà', 'Huyện Thanh Miện', 'Huyện Tứ Kỳ', 'Thành Phố Chí Linh', 'Thành Phố Hải Dương', 'Thị Xã Kinh Môn']
+h_23 = ['Huyện An Dương', 'Huyện An Lão', 'Huyện Bạch Long Vĩ', 'Huyện Cát Hải', 'Huyện Kiến Thụy', 'Huyện Thủy Nguyên', 'Huyện Tiên Lãng', 'Huyện Vĩnh Bảo', 'Quận Dương Kinh', 'Quận Hải An', 'Quận Hồng Bàng', 'Quận Kiến An', 'Quận Lê Chân', 'Quận Ngô Quyền', 'Quận Đồ Sơn']
+h_24 = ['Huyện Châu Thành', 'Huyện Châu Thành A', 'Huyện Long Mỹ', 'Huyện Phụng Hiệp', 'Huyện Vị Thủy', 'Thành Phố Ngã Bảy', 'Thành Phố Vị Thanh', 'Thị Xã Long Mỹ']
+h_25 = ['Huyện Cam Lâm', 'Huyện Diên Khánh', 'Huyện Khánh Sơn', 'Huyện Khánh Vĩnh', 'Huyện Trường Sa', 'Huyện Vạn Ninh', 'Thành Phố Cam Ranh', 'Thành Phố Nha Trang', 'Thị Xã Ninh Hòa']
+h_26 = ['Huyện An Biên', 'Huyện An Minh', 'Huyện Châu Thành', 'Huyện Giang Thành', 'Huyện Giồng Riềng', 'Huyện Gò Quao', 'Huyện Hòn Đất', 'Huyện Kiên Hải', 'Huyện Kiên Lương', 'Huyện Tân Hiệp', 'Huyện U Minh Thượng', 'Huyện Vĩnh Thuận', 'Thành Phố Hà Tiên', 'Thành Phố Phú Quốc', 'Thành Phố Rạch Giá']
+h_27 = ["Huyện Ia H' Drai", 'Huyện Kon Plông', 'Huyện Kon Rẫy', 'Huyện Ngọc Hồi', 'Huyện Sa Thầy', 'Huyện Tu Mơ Rông', 'Huyện Đắk Glei', 'Huyện Đắk Hà', 'Huyện Đắk Tô', 'Thành Phố Kon Tum']
+h_28 = ['Huyện Mường Tè', 'Huyện Nậm Nhùn', 'Huyện Phong Thổ', 'Huyện Sìn Hồ', 'Huyện Tam Đường', 'Huyện Than Uyên', 'Huyện Tân Uyên', 'Thành Phố Lai Châu']
+h_29 = ['Huyện Bến Lức', 'Huyện Châu Thành', 'Huyện Cần Giuộc', 'Huyện Cần Đước', 'Huyện Mộc Hóa', 'Huyện Thạnh Hóa', 'Huyện Thủ Thừa', 'Huyện Tân Hưng', 'Huyện Tân Thạnh', 'Huyện Tân Trụ', 'Huyện Vĩnh Hưng', 'Huyện Đức Huệ', 'Huyện Đức Hòa', 'Thành Phố Tân An', 'Thị Xã Kiến Tường']
+h_30 = ['Huyện Bát Xát', 'Huyện Bảo Thắng', 'Huyện Bảo Yên', 'Huyện Bắc Hà', 'Huyện Mường Khương', 'Huyện Si Ma Cai', 'Huyện Văn Bàn', 'Thành Phố Lào Cai', 'Thị Xã Sa Pa']
+
+
+h_31 = ['Huyện Bảo Lâm', 'Huyện Cát Tiên', 'Huyện Di Linh', 'Huyện Lâm Hà', 'Huyện Lạc Dương', 'Huyện Đam Rông', 'Huyện Đơn Dương', 'Huyện Đạ Huoai', 'Huyện Đạ Tẻh', 'Huyện Đức Trọng', 'Thành Phố Bảo Lộc', 'Thành Phố Đà Lạt']
+h_32 = ['Huyện Bình Gia', 'Huyện Bắc Sơn', 'Huyện Cao Lộc', 'Huyện Chi Lăng', 'Huyện Hữu Lũng', 'Huyện Lộc Bình', 'Huyện Tràng Định', 'Huyện Văn Lãng', 'Huyện Văn Quan', 'Huyện Đình Lập', 'Thành Phố Lạng Sơn']
+h_33 = ['Huyện Giao Thủy', 'Huyện Hải Hậu', 'Huyện Mỹ Lộc', 'Huyện Nam Trực', 'Huyện Nghĩa Hưng', 'Huyện Trực Ninh', 'Huyện Vụ Bản', 'Huyện Xuân Trường', 'Huyện Ý Yên', 'Thành Phố Nam Định']
+h_34 = ['Huyện Anh Sơn', 'Huyện Con Cuông', 'Huyện Diễn Châu', 'Huyện Hưng Nguyên', 'Huyện Kỳ Sơn', 'Huyện Nam Đàn', 'Huyện Nghi Lộc', 'Huyện Nghĩa Đàn', 'Huyện Quế Phong', 'Huyện Quỳ Châu', 'Huyện Quỳ Hợp', 'Huyện Quỳnh Lưu', 'Huyện Thanh Chương', 'Huyện Tân Kỳ', 'Huyện Tương Dương', 'Huyện Yên Thành', 'Huyện Đô Lương', 'Thành Phố Vinh', 'Thị Xã Cửa Lò', 'Thị Xã Hoàng Mai', 'Thị Xã Thái Hòa']
+h_35 = ['Huyện Gia Viễn', 'Huyện Hoa Lư', 'Huyện Kim Sơn', 'Huyện Nho Quan', 'Huyện Yên Khánh', 'Huyện Yên Mô', 'Thành Phố Ninh Bình', 'Thành Phố Tam Điệp']
+h_36 = ['Huyện Bác Ái', 'Huyện Ninh Hải', 'Huyện Ninh Phước', 'Huyện Ninh Sơn', 'Huyện Thuận Bắc', 'Huyện Thuận Nam', 'Thành Phố Phan Rang-Tháp Chàm']
+h_37 = ['Huyện Cẩm Khê', 'Huyện Hạ Hòa', 'Huyện Lâm Thao', 'Huyện Phù Ninh', 'Huyện Tam Nông', 'Huyện Thanh Ba', 'Huyện Thanh Sơn', 'Huyện Thanh Thủy', 'Huyện Tân Sơn', 'Huyện Yên Lập', 'Huyện Đoan Hùng', 'Thành Phố Việt Trì', 'Thị Xã Phú Thọ']
+h_38 = ['Huyện Phú Hòa', 'Huyện Sông Hinh', 'Huyện Sơn Hòa', 'Huyện Tuy An', 'Huyện Tây Hòa', 'Huyện Đồng Xuân', 'Thành Phố Tuy Hòa', 'Thị Xã Sông Cầu', 'Thị Xã Đông Hòa']
+h_39 = ['Huyện Bố Trạch', 'Huyện Lệ Thủy', 'Huyện Minh Hóa', 'Huyện Quảng Ninh', 'Huyện Quảng Trạch', 'Huyện Tuyên Hóa', 'Thành Phố Đồng Hới', 'Thị Xã Ba Đồn']
+h_40 = ['Huyện Bắc Trà My', 'Huyện Duy Xuyên', 'Huyện Hiệp Đức', 'Huyện Nam Giang', 'Huyện Nam Trà My', 'Huyện Nông Sơn', 'Huyện Núi Thành', 'Huyện Phú Ninh', 'Huyện Phước Sơn', 'Huyện Quế Sơn', 'Huyện Thăng Bình', 'Huyện Tiên Phước', 'Huyện Tây Giang', 'Huyện Đông Giang', 'Huyện Đại Lộc', 'Thành Phố Hội An', 'Thành Phố Tam Kỳ', 'Thị Xã Điện Bàn']
+
+
+h_41 = ['Huyện Ba Tơ', 'Huyện Bình Sơn', 'Huyện Lý Sơn', 'Huyện Minh Long', 'Huyện Mộ Đức', 'Huyện Nghĩa Hành', 'Huyện Sơn Hà', 'Huyện Sơn Tây', 'Huyện Sơn Tịnh', 'Huyện Trà Bồng', 'Huyện Tư Nghĩa', 'Thành Phố Quảng Ngãi', 'Thị Xã Đức Phổ']
+h_42 = ['Huyện Ba Chẽ', 'Huyện Bình Liêu', 'Huyện Cô Tô', 'Huyện Hải Hà', 'Huyện Tiên Yên', 'Huyện Vân Đồn', 'Huyện Đầm Hà', 'Thành Phố Cẩm Phả', 'Thành Phố Hạ Long', 'Thành Phố Móng Cái', 'Thành Phố Uông Bí', 'Thị Xã Quảng Yên', 'Thị Xã Đông Triều']
+h_43 = ['Huyện Cam Lộ', 'Huyện Cồn Cỏ', 'Huyện Gio Linh', 'Huyện Hướng Hóa', 'Huyện Hải Lăng', 'Huyện Triệu Phong', 'Huyện Vĩnh Linh', 'Huyện Đa Krông', 'Thành Phố Đông Hà', 'Thị Xã Quảng Trị']
+h_44 = ['Huyện Châu Thành', 'Huyện Cù Lao Dung', 'Huyện Kế Sách', 'Huyện Long Phú', 'Huyện Mỹ Tú', 'Huyện Mỹ Xuyên', 'Huyện Thạnh Trị', 'Huyện Trần Đề', 'Thành Phố Sóc Trăng', 'Thị Xã Ngã Năm', 'Thị Xã Vĩnh Châu']
+h_45 = ['Huyện Bắc Yên', 'Huyện Mai Sơn', 'Huyện Mường La', 'Huyện Mộc Châu', 'Huyện Phù Yên', 'Huyện Quỳnh Nhai', 'Huyện Sông Mã', 'Huyện Sốp Cộp', 'Huyện Thuận Châu', 'Huyện Vân Hồ', 'Huyện Yên Châu', 'Thành Phố Sơn La']
+h_46 = ['Huyện Bình Chánh', 'Huyện Cần Giờ', 'Huyện Củ Chi', 'Huyện Hóc Môn', 'Huyện Nhà Bè', 'Quận 1', 'Quận 10', 'Quận 11', 'Quận 12', 'Quận 3', 'Quận 4', 'Quận 5', 'Quận 6', 'Quận 7', 'Quận 8', 'Quận Bình Thạnh', 'Quận Bình Tân', 'Quận Gò Vấp', 'Quận Phú Nhuận', 'Quận Tân Bình', 'Quận Tân Phú', 'Thành Phố Thủ Đức']
+h_47 = ['Huyện Bá Thước', 'Huyện Cẩm Thủy', 'Huyện Hoằng Hóa', 'Huyện Hà Trung', 'Huyện Hậu Lộc', 'Huyện Lang Chánh', 'Huyện Mường Lát', 'Huyện Nga Sơn', 'Huyện Ngọc Lặc', 'Huyện Như Thanh', 'Huyện Như Xuân', 'Huyện Nông Cống', 'Huyện Quan Hóa', 'Huyện Quan Sơn', 'Huyện Quảng Xương', 'Huyện Thiệu Hóa', 'Huyện Thường Xuân', 'Huyện Thạch Thành', 'Huyện Thọ Xuân', 'Huyện Triệu Sơn', 'Huyện Vĩnh Lộc', 'Huyện Yên Định', 'Huyện Đông Sơn', 'Thành Phố Sầm Sơn', 'Thành Phố Thanh Hóa', 'Thị Xã Bỉm Sơn', 'Thị Xã Nghi Sơn']
+h_48 = ['Huyện Hưng Hà', 'Huyện Kiến Xương', 'Huyện Quỳnh Phụ', 'Huyện Thái Thụy', 'Huyện Tiền Hải', 'Huyện Vũ Thư', 'Huyện Đông Hưng', 'Thành Phố Thái Bình']
+h_49 = ['Huyện Phú Bình', 'Huyện Phú Lương', 'Huyện Võ Nhai', 'Huyện Đại Từ', 'Huyện Định Hóa', 'Huyện Đồng Hỷ', 'Thành Phố Phổ Yên', 'Thành Phố Sông Công', 'Thành Phố Thái Nguyên']
+h_50 = ['Huyện A Lưới', 'Huyện Nam Đông', 'Huyện Phong Điền', 'Huyện Phú Lộc', 'Huyện Phú Vang', 'Huyện Quảng Điền', 'Thành Phố Huế', 'Thị Xã Hương Thủy', 'Thị Xã Hương Trà']
+
+h_51 = ['Huyện Cai Lậy', 'Huyện Châu Thành', 'Huyện Chợ Gạo', 'Huyện Cái Bè', 'Huyện Gò Công Tây', 'Huyện Gò Công Đông', 'Huyện Tân Phú Đông', 'Huyện Tân Phước', 'Thành Phố Mỹ Tho', 'Thị Xã Cai Lậy', 'Thị Xã Gò Công']
+h_52 = ['Huyện Châu Thành', 'Huyện Càng Long', 'Huyện Cầu Kè', 'Huyện Cầu Ngang', 'Huyện Duyên Hải', 'Huyện Tiểu Cần', 'Huyện Trà Cú', 'Thành Phố Trà Vinh', 'Thị Xã Duyên Hải']
+h_53 = ['Huyện Chiêm Hóa', 'Huyện Hàm Yên', 'Huyện Lâm Bình', 'Huyện Na Hang', 'Huyện Sơn Dương', 'Huyện Yên Sơn', 'Thành Phố Tuyên Quang']
+h_54 = ['Huyện Bến Cầu', 'Huyện Châu Thành', 'Huyện Dương Minh Châu', 'Huyện Gò Dầu', 'Huyện Tân Biên', 'Huyện Tân Châu', 'Thành Phố Tây Ninh', 'Thị Xã Hòa Thành', 'Thị Xã Trảng Bàng']
+h_55 = ['Huyện Bình Tân', 'Huyện Long Hồ', 'Huyện Mang Thít', 'Huyện Tam Bình', 'Huyện Trà Ôn', 'Huyện Vũng Liêm', 'Thành Phố Vĩnh Long', 'Thị Xã Bình Minh']
+h_56 = ['Huyện Bình Xuyên', 'Huyện Lập Thạch', 'Huyện Sông Lô', 'Huyện Tam Dương', 'Huyện Tam Đảo', 'Huyện Vĩnh Tường', 'Huyện Yên Lạc', 'Thành Phố Phúc Yên', 'Thành Phố Vĩnh Yên']
+h_57 = ['Huyện Lục Yên', 'Huyện Mù Căng Chải', 'Huyện Trạm Tấu', 'Huyện Trấn Yên', 'Huyện Văn Chấn', 'Huyện Văn Yên', 'Huyện Yên Bình', 'Thành Phố Yên Bái', 'Thị Xã Nghĩa Lộ']
+h_58 = ['Huyện Mường Chà', 'Huyện Mường Nhé', 'Huyện Mường Ảng', 'Huyện Nậm Pồ', 'Huyện Tuần Giáo', 'Huyện Tủa Chùa', 'Huyện Điện Biên', 'Huyện Điện Biên Đông', 'Thành Phố Điện Biên Phủ', 'Thị Xã Mường Lay']
+h_59 = ['Huyện Hòa Vang', 'Quận Cẩm Lệ', 'Quận Hải Châu', 'Quận Liên Chiểu', 'Quận Ngũ Hành Sơn', 'Quận Sơn Trà', 'Quận Thanh Khê']
+h_60 = ['Huyện Buôn Đôn', 'Huyện Cư Kuin', "Huyện Cư M'Gar", "Huyện Ea H'Leo", 'Huyện Ea Kar', 'Huyện Ea Súp', 'Huyện Krông A Na', 'Huyện Krông Bông', 'Huyện Krông Búk', 'Huyện Krông Năng', 'Huyện Krông Pắc', 'Huyện Lắk', "Huyện M'Đrắk", 'Thành Phố Buôn Ma Thuột', 'Thị Xã Buôn Hồ']
+
+
+h_61 = ['Huyện Cư Jút', 'Huyện Krông Nô', 'Huyện Tuy Đức', 'Huyện Đăk Glong', 'Huyện Đắk Mil', "Huyện Đắk R'Lấp", 'Huyện Đắk Song', 'Thành Phố Gia Nghĩa']
+h_62 = ['Huyện Cẩm Mỹ', 'Huyện Long Thành', 'Huyện Nhơn Trạch', 'Huyện Thống Nhất', 'Huyện Trảng Bom', 'Huyện Tân Phú', 'Huyện Vĩnh Cửu', 'Huyện Xuân Lộc', 'Huyện Định Quán', 'Thành Phố Biên Hòa', 'Thành Phố Long Khánh']
+h_63 = ['Huyện Cao Lãnh', 'Huyện Châu Thành', 'Huyện Hồng Ngự', 'Huyện Lai Vung', 'Huyện Lấp Vò', 'Huyện Tam Nông', 'Huyện Thanh Bình', 'Huyện Tháp Mười', 'Huyện Tân Hồng', 'Thành Phố Cao Lãnh', 'Thành Phố Hồng Ngự', 'Thành Phố Sa Đéc']
+
+
+
+x_1_1 = ['Thị Trấn An Phú', 'Thị Trấn Long Bình', 'Xã Khánh An', 'Xã Khánh Bình', 'Xã Nhơn Hội', 'Xã Phú Hội', 'Xã Phú Hữu', 'Xã Phước Hưng', 'Xã Quốc Thái', 'Xã Vĩnh Hậu', 'Xã Vĩnh Hội Đông', 'Xã Vĩnh Lộc', 'Xã Vĩnh Trường', 'Xã Đa Phước']
+x_1_2 = ['Thị Trấn Cái Dầu', 'Thị Trấn Vĩnh Thạnh Trung', 'Xã Bình Chánh', 'Xã Bình Long', 'Xã Bình Mỹ', 'Xã Bình Phú', 'Xã Bình Thủy', 'Xã Khánh Hòa', 'Xã Mỹ Phú', 'Xã Mỹ Đức', 'Xã Thạnh Mỹ Tây', 'Xã Ô Long Vỹ', 'Xã Đào Hữu Cảnh']
+x_1_3 = ['Thị Trấn An Châu', 'Thị Trấn Vĩnh Bình', 'Xã An Hòa', 'Xã Bình Hòa', 'Xã Bình Thạnh', 'Xã Cần Đăng', 'Xã Hòa Bình Thạnh', 'Xã Tân Phú', 'Xã Vĩnh An', 'Xã Vĩnh Hanh', 'Xã Vĩnh Lợi', 'Xã Vĩnh Nhuận', 'Xã Vĩnh Thành']
+x_1_4 = ['Thị Trấn Chợ Mới', 'Thị Trấn Mỹ Luông', 'Xã An Thạnh Trung', 'Xã Bình Phước Xuân', 'Xã Hòa An', 'Xã Hòa Bình', 'Xã Hội An', 'Xã Kiến An', 'Xã Kiến Thành', 'Xã Long Giang', 'Xã Long Kiến', 'Xã Long Điền A', 'Xã Long Điền B', 'Xã Mỹ An', 'Xã Mỹ Hiệp', 'Xã Mỹ Hội Đông', 'Xã Nhơn Mỹ', 'Xã Tấn Mỹ']
+x_1_5 = ['Thị Trấn Chợ Vàm', 'Thị Trấn Phú Mỹ', 'Xã Bình Thạnh Đông', 'Xã Hiệp Xương', 'Xã Hòa Lạc', 'Xã Long Hòa', 'Xã Phú An', 'Xã Phú Bình', 'Xã Phú Hiệp', 'Xã Phú Hưng', 'Xã Phú Long', 'Xã Phú Lâm', 'Xã Phú Thành', 'Xã Phú Thạnh', 'Xã Phú Thọ', 'Xã Phú Xuân', 'Xã Tân Hòa', 'Xã Tân Trung']
+x_1_6 = ['Thị Trấn Núi Sập', 'Thị Trấn Phú Hòa', 'Thị Trấn Óc Eo', 'Xã An Bình', 'Xã Bình Thành', 'Xã Mỹ Phú Đông', 'Xã Phú Thuận', 'Xã Thoại Giang', 'Xã Tây Phú', 'Xã Vĩnh Chánh', 'Xã Vĩnh Khánh', 'Xã Vĩnh Phú', 'Xã Vĩnh Trạch', 'Xã Vọng Thê', 'Xã Vọng Đông', 'Xã Định Mỹ', 'Xã Định Thành']
+x_1_7 = ['Thị Trấn Ba Chúc', 'Thị Trấn Cô Tô', 'Thị Trấn Tri Tôn', 'Xã An Tức', 'Xã Châu Lăng', 'Xã Lê Trì', 'Xã Lương An Trà', 'Xã Lương Phi', 'Xã Lạc Quới', 'Xã Núi Tô', 'Xã Tà Đảnh', 'Xã Tân Tuyến', 'Xã Vĩnh Gia', 'Xã Vĩnh Phước', 'Xã Ô Lâm']
+x_1_8 = ['Thị Trấn Chi Lăng', 'Thị Trấn Nhà Bàng', 'Thị Trấn Tịnh Biên', 'Xã An Cư', 'Xã An Hảo', 'Xã An Nông', 'Xã An Phú', 'Xã Nhơn Hưng', 'Xã Núi Voi', 'Xã Thới Sơn', 'Xã Tân Lập', 'Xã Tân Lợi', 'Xã Văn Giáo', 'Xã Vĩnh Trung']
+x_1_9 = ['Phường Châu Phú A', 'Phường Châu Phú B', 'Phường Núi Sam', 'Phường Vĩnh Mỹ', 'Phường Vĩnh Ngươn', 'Xã Vĩnh Châu', 'Xã Vĩnh Tế']
+x_1_10 = ['Phường Bình Khánh', 'Phường Bình Đức', 'Phường Mỹ Bình', 'Phường Mỹ Hòa', 'Phường Mỹ Long', 'Phường Mỹ Phước', 'Phường Mỹ Quý', 'Phường Mỹ Thạnh', 'Phường Mỹ Thới', 'Phường Mỹ Xuyên', 'Phường Đông Xuyên', 'Xã Mỹ Hòa Hưng', 'Xã Mỹ Khánh']
+x_1_11 = ['Phường Long Châu', 'Phường Long Hưng', 'Phường Long Phú', 'Phường Long Sơn', 'Phường Long Thạnh', 'Xã Châu Phong', 'Xã Long An', 'Xã Lê Chánh', 'Xã Phú Lộc', 'Xã Phú Vĩnh', 'Xã Tân An', 'Xã Tân Thạnh', 'Xã Vĩnh Hòa', 'Xã Vĩnh Xương']
