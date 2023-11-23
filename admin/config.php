f078d3874913cdf9cc289a305757febb2503a862
paddington2003 <phuonganhduongg2211@gmail.com>
2023-11-23 15:48:59 +0700

    xong phan update

--- admin/config.php
+++ admin/config.php
@@ -2,7 +2,7 @@
   $servername = "localhost";
   $username = "root";
   $password = "";
-  $db = "shopee";
+  $db = "shoppe_new";
 
   $conn = mysqli_connect($servername,$username,$password,$db);
   //  Check connection
